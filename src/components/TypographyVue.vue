c4b86f870608963f68ce9f21f84349b0583e4c90
HyunHoo Im <litinglabs03@gmail.com>
2024-04-19 14:32:29 +0900

    typography

---
+++ src/components/TypographyVue.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="text-h1">text-h1</div>
+    <div class="text-h2">text-h2</div>
+    <div class="text-h3">text-h3</div>
+    <div class="text-h4">text-h4</div>
+    <div class="text-h5">text-h5</div>
+    <div class="text-h6">text-h6</div>
+    <div class="text-subtitle1">text-subtitle1</div>
+    <div class="text-subtitle2">text-subtitle2</div>
+    <div class="text-body1">text-body1</div>
+    <div class="text-body2">text-body2</div>
+    <div class="text-caption">text-caption</div>
+    <div class="text-overline">text-overline</div>
+  </div>
+  <div>
+    <div class="text-weight-thin">text-weight-thin</div>
+    <div class="text-weight-light">text-weight-light</div>
+    <div class="text-weight-regular">text-weight-regular</div>
+    <div class="text-weight-medium">text-weight-medium</div>
+    <div class="text-weight-bold">text-weight-bold</div>
+    <div class="text-weight-bolder">text-weight-bolder</div>
+  </div>
+  <div>
+    <div class="text-right">text-right</div>
+    <div class="text-left">text-left</div>
+    <div class="text-center">text-center</div>
+    <div class="text-justify">text-justify</div>
+    <div class="text-bold">text-bold</div>
+    <div class="text-italic">text-italic</div>
+    <div class="text-no-wtext-no">테트리스</div>
+    <div class="text-strike">text-strike</div>
+    <div class="text-uppercase">text-uppercase</div>
+    <div class="text-lowercase">text-lowercase</div>
+    <div class="text-capitalize">text-capitalize</div>
+  </div>
+</template>
